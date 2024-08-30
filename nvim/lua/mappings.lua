667033dc614a1885c570190d27fa944f5ddb7392
marl8n <ivandmarlon@gmail.com>
2024-08-30 10:01:18 -0600

    Added mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -6,4 +6,11 @@ local opts = { noremap = true, silent = true }
 -- NERDTree toggle
 map('n', '<leader>nt', ':NERDTreeToggle<CR>', opts)
 
+-- Custom mappings
+map('n', '<leader>pv', ':Ex<CR>', opts)
+map('n', '<leader>x', ':x<CR>', opts)
+map('n', '<leader>w', ':w<CR>', opts)
+map('n', '<leader>q', ':q<CR>', opts)
+map('n', '<leader>q!', ':qa!<CR>', opts)
+
 -- LSP key mappings will be set in the lsp.lua file

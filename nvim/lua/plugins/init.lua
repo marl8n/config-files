c4c1fc8ab2f75c99371fa4452f2466300279864c
marl8n <ivandmarlon@gmail.com>
2024-09-03 07:33:29 -0600

    Implemented live_grep_args

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -21,7 +21,10 @@ return require('packer').startup(function(use)
 
   use {
       'nvim-telescope/telescope.nvim',
-      requires = { {'nvim-lua/plenary.nvim'} }
+      requires = {
+          {'nvim-lua/plenary.nvim'},
+          {'nvim-telescope/telescope-live-grep-args.nvim'}
+      }
   }
 
   -- LSP and Autocompletion
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -13,8 +13,11 @@ telescope.setup{
   }
 }
 
+-- Load the extension
+telescope.load_extension('live_grep_args')
+
 -- Key mappings for Telescope
 vim.keymap.set('n', '<leader>ff', require('telescope.builtin').find_files, { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>fg', require('telescope.builtin').live_grep, { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>fg', require('telescope').extensions.live_grep_args.live_grep_args, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fb', require('telescope.builtin').buffers, { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>fh', require('telescope.builtin').help_tags, { noremap = true, silent = true })
